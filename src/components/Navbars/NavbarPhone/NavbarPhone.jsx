e13c82f4e0d9adfd2a47cfa15a60f4a98fbb2ef4
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-16 11:06:29 -0700

    updated

--- src/components/Navbars/NavbarPhone/NavbarPhone.jsx
+++ src/components/Navbars/NavbarPhone/NavbarPhone.jsx
@@ -1,14 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import gsap from "gsap";
 import hamburger from "../../../assets/menu.png";
 import logo from "../../../assets/solarLogo.jpeg";
 import "./navbarphone.css";
 
 export function NavbarPhone() {
   const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+  function handleActivateHamburger() {
+    if (!isHamburgerActive) {
+      setIsHamburgerActive(true);
+    } else if (isHamburgerActive) {
+      gsap.to(".navbar-phone-dropdown-main-container", {
+        height: 0,
+        opacity: 0,
+        duration: 1.5,
+        ease: "power4.out",
+      });
+      setTimeout(() => {
+        setIsHamburgerActive(false);
+      }, 1000);
+    }
+  }
+
+  useEffect(() => {
+    if (isHamburgerActive) {
+      gsap.from(".navbar-phone-dropdown-main-container", {
+        height: 0,
+        opacity: 0,
+        duration: 1.5,
+        ease: "power4.out",
+      });
+    }
+  }, [isHamburgerActive]);
+
+  const links = [
+    { linkName: "About Us" },
+    { linkName: "Product" },
+    { linkName: "Services" },
+    { linkName: "Locations" },
+    { linkName: "Contact Us" },
+  ];
+  console.log(isHamburgerActive);
   return (
     <div className="navbar-phone-main-container">
       <img src={logo} width="200" />
-      <img src={hamburger} width="50" className="navbar-phone-hamburger" />
+      <img
+        src={hamburger}
+        width="50"
+        className="navbar-phone-hamburger"
+        onClick={() => handleActivateHamburger()}
+      />
+      {isHamburgerActive && (
+        <div className="navbar-phone-dropdown-main-container">
+          {links.map((link, index) => {
+            return (
+              <div key={link.linkName}>
+                <h3>{link.linkName}</h3>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
