013993123d14abc358b6e92afa594fc844b3c4c2
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-18 09:37:40 -0700

    updated

--- src/components/Navbars/NavbarComp/NavbarComp.jsx
+++ src/components/Navbars/NavbarComp/NavbarComp.jsx
@@ -1,3 +1,28 @@
+import logo from "../../../assets/solarLogo.jpeg";
+import "./navbarcomp.css";
+
 export function NavbarComp() {
-  return <h1>computer</h1>;
+  const links = [
+    { linkName: "About Us" },
+    { linkName: "Product" },
+    { linkName: "Services" },
+    { linkName: "Locations" },
+    { linkName: "Contact Us" },
+  ];
+  return (
+    <div className="navbar-comp-main-container">
+      <div className="navbar-comp-logo-container">
+        <img src={logo} className="navbar-comp-logo" />
+      </div>
+      <div className="navbar-comp-link-container">
+        {links.map((link, index) => {
+          return (
+            <h3 className="navbar-comp-link" key={link.linkName}>
+              {link.linkName}
+            </h3>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
