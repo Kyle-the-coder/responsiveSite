4aff9cb445f89967ba191e4d135fe179b5f839ab
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-18 11:02:05 -0700

    updated

--- src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
+++ src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
@@ -5,6 +5,11 @@ export function LandingPagePhone() {
   return (
     <div className="landing-page-phone-main-container">
       <img src={bgImage} className="landing-page-phone-bg-image" />
+      <div className="landing-page-phone-main-quote-container">
+        <h1>
+          Exclusive <span>Solar</span> Appointments
+        </h1>
+      </div>
     </div>
   );
 }
