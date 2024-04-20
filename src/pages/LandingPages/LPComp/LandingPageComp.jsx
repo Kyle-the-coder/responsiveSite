ac2cb4ae856bc620f07579887899fbfb9ff6718c
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-20 11:54:51 -0700

    updated

--- src/pages/LandingPages/LPComp/LandingPageComp.jsx
+++ src/pages/LandingPages/LPComp/LandingPageComp.jsx
@@ -1,7 +1,3 @@
 export function LandingPageComp() {
-  return (
-    <div>
-      <spline-viewer url="https://prod.spline.design/w042fzE4JUnZRDlq/scene.splinecode"></spline-viewer>
-    </div>
-  );
+  return <div></div>;
 }
--- src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
+++ src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
@@ -4,25 +4,34 @@ import "./landingpagephone.css";
 export function LandingPagePhone() {
   return (
     <div className="landing-page-phone-main-container">
-      <img src={bgImage} className="landing-page-phone-bg-image" />
-      <div className="vignette-overlay"></div>
-      <div className="landing-page-phone-quote-container">
-        <h1 className="f2-5">
-          Exclusive <span className="orangeText">Solar </span> Appointments
-        </h1>
+      <div className="landing-page-opening-container">
+        <img src={bgImage} className="landing-page-phone-bg-image" />
+        <div className="vignette-overlay"></div>
+        <div className="landing-page-phone-quote-container">
+          <h1 className="f2-5">
+            Exclusive <span className="orangeText">Solar </span> Appointments
+          </h1>
+        </div>
+
+        <div className="landing-page-phone-quote-container">
+          <h4>
+            Let us fill your sales pipeline with already interested homeowner's
+            waiting to buy solar
+          </h4>
+        </div>
+
+        <div className="landing-page-phone-quote-container">
+          <button className="landing-page-phone-button">
+            Book a Strategy Call
+          </button>
+        </div>
       </div>
 
-      <div className="landing-page-phone-quote-container">
-        <h4>
-          Let us fill your sales pipeline with already interested homeowner's
-          waiting to buy solar
-        </h4>
-      </div>
-
-      <div className="landing-page-phone-quote-container">
-        <button className="landing-page-phone-button">
-          Book a Strategy Call
-        </button>
+      <div className="landing-page-3D-container">
+        <spline-viewer
+          loading-anim-type="spinner-small-dark"
+          url="https://prod.spline.design/w042fzE4JUnZRDlq/scene.splinecode"
+        ></spline-viewer>
       </div>
     </div>
   );
