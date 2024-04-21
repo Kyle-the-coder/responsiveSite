67104b560690c6bcb320aa86616a1b70422b5035
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-21 10:09:06 -0700

    updated

--- src/pages/LandingPages/LPComp/LandingPageComp.jsx
+++ src/pages/LandingPages/LPComp/LandingPageComp.jsx
@@ -1,3 +1,10 @@
 export function LandingPageComp() {
-  return <div></div>;
+  return (
+    <div className="landing-page-comp-main-container">
+      <spline-viewer
+        loading-anim-type="spinner-small-dark"
+        url="https://prod.spline.design/w042fzE4JUnZRDlq/scene.splinecode"
+      ></spline-viewer>
+    </div>
+  );
 }
