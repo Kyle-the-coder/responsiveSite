e87fdaf05aeb020f879c08f81b9575d0ae197e69
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-22 10:04:08 -0700

    updated

--- src/components/Navbars/NavbarPhone/NavbarPhone.jsx
+++ src/components/Navbars/NavbarPhone/NavbarPhone.jsx
@@ -3,9 +3,12 @@ import gsap from "gsap";
 import hamburger from "../../../assets/menu.png";
 import logo from "../../../assets/solarLogo.jpeg";
 import "./navbarphone.css";
+import { useNavigate } from "react-router-dom";
 
 export function NavbarPhone() {
   const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+  const navigate = useNavigate();
 
   function handleActivateHamburger() {
     if (!isHamburgerActive) {
@@ -23,6 +26,34 @@ export function NavbarPhone() {
     }
   }
 
+  function handleNavigate(link) {
+    if (link === "/aboutUs") {
+      navigate(link);
+      gsap.to(".navbar-phone-dropdown-main-container", {
+        height: 0,
+        opacity: 0,
+        duration: 1,
+        ease: "power4.out",
+      });
+      setTimeout(() => {
+        setIsHamburgerActive(false);
+      }, 300);
+    }
+  }
+
+  function handleHomeNavigate() {
+    navigate("/");
+    gsap.to(".navbar-phone-dropdown-main-container", {
+      height: 0,
+      opacity: 0,
+      duration: 1,
+      ease: "power4.out",
+    });
+    setTimeout(() => {
+      setIsHamburgerActive(false);
+    }, 300);
+  }
+
   useEffect(() => {
     if (isHamburgerActive) {
       gsap.from(".navbar-phone-dropdown-main-container", {
@@ -35,7 +66,7 @@ export function NavbarPhone() {
   }, [isHamburgerActive]);
 
   const links = [
-    { linkName: "About Us" },
+    { linkName: "About Us", link: "/aboutUs" },
     { linkName: "Product" },
     { linkName: "Services" },
     { linkName: "Locations" },
@@ -44,7 +75,7 @@ export function NavbarPhone() {
 
   return (
     <div className="navbar-phone-main-container">
-      <img src={logo} width="200" />
+      <img src={logo} width="200" onClick={() => handleHomeNavigate()} />
       <img
         src={hamburger}
         width="50"
@@ -56,7 +87,12 @@ export function NavbarPhone() {
           {links.map((link, index) => {
             return (
               <div key={link.linkName}>
-                <h3 className="dropdown-link-name">{link.linkName}</h3>
+                <h3
+                  className="dropdown-link-name"
+                  onClick={() => handleNavigate(link.link)}
+                >
+                  {link.linkName}
+                </h3>
               </div>
             );
           })}
