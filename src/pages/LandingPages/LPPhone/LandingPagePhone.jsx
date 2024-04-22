764827958137967ef88aa40e439478854c548e1d
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-22 09:33:36 -0700

    updated

--- src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
+++ src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
@@ -10,20 +10,20 @@ export function LandingPagePhone() {
         <img src={bgImage} className="landing-page-phone-bg-image" />
         <div className="vignette-overlay"></div>
         <div className="landing-page-phone-quote-container">
-          <h1 className="f2-5">
+          <h1 className="f3-5 m0">
             Exclusive <span className="orangeText">Solar </span> Appointments
           </h1>
         </div>
 
         <div className="landing-page-phone-quote-container">
-          <h4>
+          <h4 className="f1-5 m0">
             Let us fill your sales pipeline with already interested homeowner's
             waiting to buy solar
           </h4>
         </div>
 
         <div className="landing-page-phone-quote-container">
-          <button className="landing-page-phone-button">
+          <button className="m0 landing-page-phone-button">
             Book a Strategy Call
           </button>
         </div>
