1c216eedc275ad545da1877c5c6594af1699253b
Kyle-the-coder <kylethacoder@gmail.com>
2024-04-24 11:19:53 -0700

    updated

--- src/pages/LandingPages/LPComp/LandingPageComp.jsx
+++ src/pages/LandingPages/LPComp/LandingPageComp.jsx
@@ -3,10 +3,7 @@ import "./landingpagecomp.css";
 export function LandingPageComp() {
   return (
     <div className="landing-page-comp-main-container">
-      <spline-viewer
-        loading-anim-type="spinner-small-dark"
-        url="https://prod.spline.design/w042fzE4JUnZRDlq/scene.splinecode"
-      ></spline-viewer>
+      <spline-viewer url="https://prod.spline.design/QdyM5f8zkxWneOeu/scene.splinecode"></spline-viewer>
     </div>
   );
 }
--- src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
+++ src/pages/LandingPages/LPPhone/LandingPagePhone.jsx
@@ -39,10 +39,7 @@ export function LandingPagePhone() {
       </div>
 
       <div className="landing-page-3D-container">
-        <spline-viewer
-          loading-anim-type="spinner-small-dark"
-          url="https://prod.spline.design/w042fzE4JUnZRDlq/scene.splinecode"
-        ></spline-viewer>
+        <spline-viewer url="https://prod.spline.design/QdyM5f8zkxWneOeu/scene.splinecode"></spline-viewer>
       </div>
     </div>
   );
