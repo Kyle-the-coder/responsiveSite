3cb0dbb4da7add294d8561564d311c8a4dd9de6b
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-05 09:49:42 -0700

    updated

--- src/pages/LandingPages/LPComp/LandingPageComp.jsx
+++ src/pages/LandingPages/LPComp/LandingPageComp.jsx
@@ -1,9 +1,5 @@
 import "./landingpagecomp.css";
 
 export function LandingPageComp() {
-  return (
-    <div className="landing-page-comp-main-container">
-      <spline-viewer url="https://prod.spline.design/fhssD-863C9KnEe9/scene.splinecode"></spline-viewer>
-    </div>
-  );
+  return <div className="landing-page-comp-main-container"></div>;
 }
