5501bb8152337a5ed144df75bfb8aadd83135a44
Kyle-the-coder <kylethacoder@gmail.com>
2024-05-08 10:39:41 -0700

    updated

--- src/components/LottieAnimation.jsx
+++ src/components/LottieAnimation.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import lottie from "lottie-web";
-import lottieFile from "../assets/lottieFile.json"; // Import your Lottie animation JSON file
+import lottieFile from "../assets/lottieFile.json";
 
 function LottieAnimation({ isHamburgerActive, isAnimationActive }) {
   const containerRef = useRef(null);
@@ -10,27 +10,25 @@ function LottieAnimation({ isHamburgerActive, isAnimationActive }) {
   console.log("ham", isHamburgerActive);
 
   useEffect(() => {
-    // Initialize Lottie animation
     animRef.current = lottie.loadAnimation({
       container: containerRef.current,
-      renderer: "svg", // Specify the renderer
+      renderer: "svg",
       loop: false,
       autoplay: false,
-      animationData: lottieFile, // Your animation data
+      animationData: lottieFile,
     });
 
-    // Clean up animation when component unmounts
     return () => {
       if (animRef.current) {
         animRef.current.destroy();
       }
     };
-  }, []); // Empty dependency array ensures useEffect runs only once
+  }, []);
 
   useEffect(() => {
     const currentFrame = animRef.current.currentFrame;
+
     if (isAnimationActive === false) {
-      // animRef.current.playSegments([0, 43], true);
       animRef.current.playSegments([currentFrame, 0], true);
     } else if (isHamburgerActive) {
       animRef.current.playSegments([0, 43], true);
@@ -38,9 +36,7 @@ function LottieAnimation({ isHamburgerActive, isAnimationActive }) {
   }, [isHamburgerActive, isAnimationActive]);
 
   return (
-    <div ref={containerRef} style={{ width: "100%", height: "100%" }}>
-      {/* This div will contain the Lottie animation */}
-    </div>
+    <div ref={containerRef} style={{ width: "100%", height: "100%" }}></div>
   );
 }
 
